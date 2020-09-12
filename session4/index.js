feb5c2ca257c84b74789827f4d01f679c4e97180
eugeneZolotarenko <zolotarenko.yevheniy1998@gmail.com>
2020-09-12 14:59:33 +0200

    Added new task and solved

--- session4/index.js
+++ session4/index.js
@@ -45,3 +45,18 @@ console.log(
     { milk: 5, sugar: 120, butter: 500 }
   )
 )
+
+/**
+Create a function that returns the maximum number of weach array in main array.
+*/
+
+const arr = [
+  [10, 12, 38, 84, 34],
+  [9, 99, 23, 65, 32],
+  [4, 21, 49, 22, 24],
+  [92, 34, 24, 12, 1],
+]
+
+const arrOfMaxValues = Array.from(arr.map((arr) => Math.max(...arr)))
+
+console.log(arrOfMaxValues)
